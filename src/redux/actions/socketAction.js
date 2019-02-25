1c766f1ba8873598a8dbe5a309fb1f31a07207e0
Nick An <modaristis@gmail.com>
2019-02-25 04:39:43 +0200

    removed unused actions and packages

--- src/redux/actions/socketAction.js
+++ src/redux/actions/socketAction.js
@@ -38,28 +38,3 @@ export const socketSendMessage = (message) => {
         message
     })
 }
-
-// export const startChannel = () => ({type: types.START_CHANNEL});
-// export const stopChannel = () => ({type: types.STOP_CHANNEL});
-
-// import { createAction } from 'redux-act';
-
-// export const socketConnect = createAction(types.sLOGIN);
-// export const socketDisconnect = createAction(types.sLOGOUT);
-
-// export const socketAddUser = createAction(types.sADD_USER);
-// export const socketRemoveUser = createAction(types.sREMOVE_USER);
-
-// export const socketNewMessage = (message) => {
-//     return({
-//         type: types.sNEW_MESSAGE,
-//         message
-//     })
-// }
-// export const socketSendMessage = createAction(types.sSEND_MESSAGE);
-
-export const socketConnected = () => {
-    return({
-        type: types.sCONNECTED
-    })
-}
